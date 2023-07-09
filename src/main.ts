f4b310d03f596fd1dc2f33260618057591164269
BackRunner <dev@backrunner.top>
2023-07-10 01:41:32 +0800

    feat: better type infer

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,9 @@ import { UserDataStorageOptions } from './types';
 
 export type UserDataStorageState<K> = K extends Record<string, any> ? K : Record<string, unknown>;
 
-export class UserDataStorage<K> {
+export class UserDataStorage<K extends Record<string, unknown>> {
   // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-  public state = {} as UserDataStorageState<K>;
+  public state = {} as K;
 
   private storageDir: string;
   private storageFilePath: string;
@@ -34,17 +34,17 @@ export class UserDataStorage<K> {
     }
   }
 
-  public async get(key: string) {
-    await lock(key);
+  public async get<T extends keyof K>(key: T): Promise<K[T]> {
+    await lock(key as string);
     const state = this.state[key];
-    unlock(key);
+    unlock(key as string);
     return state;
   }
 
-  public async set(key: string, value: unknown) {
-    await lock(key);
+  public async set<T extends keyof K>(key: T, value: K[T]) {
+    await lock(key as string);
     this.state[key] = value;
-    unlock(key);
+    unlock(key as string);
     this.persist();
   }
 
@@ -55,22 +55,22 @@ export class UserDataStorage<K> {
     unlock(key);
   }
 
-  public getSync(key: string) {
+  public getSync<T extends keyof K>(key: T) {
     return this.state[key];
   }
 
-  public setSync(key: string, value: unknown) {
-    lockSync(key);
+  public setSync<T extends keyof K>(key: T, value: K[T]) {
+    lockSync(key as string);
     this.state[key] = value;
     this.persist();
-    unlock(key);
+    unlock(key as string);
   }
 
-  public removeSync(key: string) {
-    lockSync(key);
+  public removeSync<T extends keyof K>(key: T) {
+    lockSync(key as string);
     delete this.state[key];
     this.persist();
-    unlock(key);
+    unlock(key as string);
   }
 
   public getState() {
