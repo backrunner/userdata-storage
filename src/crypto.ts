c4e68f9c76fd1804f88bb0c3d614f994216b6d24
backrunner <dev@backrunner.top>
2023-07-14 15:17:46 +0800

    fix: 修复加解密

--- src/crypto.ts
+++ src/crypto.ts
@@ -5,16 +5,12 @@ const keyByteLength = 32;
 const ivByteLength = 16;
 
 function getKeyAndIvFromPassword(password: string): { key: Buffer; iv: Buffer } {
-  // Create a SHA-256 hash of the password.
-  // The hash is 32 bytes long, so it can be used as the key.
-  // The IV length for AES-256-CBC is 128 bits (16 bytes).
-  // So, take the first 16 bytes of the hash as the IV.
   const hash = crypto.createHash('sha256');
   hash.update(password);
   const digest = hash.digest();
   return {
-    key: digest.slice(0, keyByteLength),
-    iv: digest.slice(keyByteLength, keyByteLength + ivByteLength),
+    key: digest,
+    iv: digest.slice(keyByteLength - ivByteLength, keyByteLength),
   };
 }
 
